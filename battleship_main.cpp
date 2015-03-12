df8e5963545a9f6bd157a0262e177926f9106af2
Zach Goins <zach.a.goins@gmail.com>
2015-03-12 11:58:57 -0400

    Nothing added but file created

---
+++ battleship_main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[1]){
+	// argc is for checking command line argument existence
+	// argv is for accessing command line arguments
+}
