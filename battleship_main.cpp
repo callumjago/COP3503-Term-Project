af8ad760923af9226366086663803fb5f24cdd2b
Zach Goins <zach.a.goins@gmail.com>
2015-03-17 18:25:08 -0400

    Added printout array

--- battleship_main.cpp
+++ battleship_main.cpp
@@ -8,4 +8,24 @@ using namespace std;
 int main(int argc, char *argv[1]){
 	// argc is for checking command line argument existence
 	// argv is for accessing command line arguments
+
+	// For right now this will create a 10x10 array 
+	// It only fills it with numbers right now though so we need to 
+	// find a way to fill it with a letter and number
+
+	int values[10][10];
+	int count = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		
+		for (int j = 0; j < 10; j++)
+		{
+			values[i][j] = count;
+			cout << values[i][j] << " ";
+			count++;
+		}
+		cout << "\n";
+
+	}
 }
