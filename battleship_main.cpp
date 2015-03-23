860f35f1e1fa63dc0a690206e7299e0c05e8eb19
Zach Goins <zach.a.goins@gmail.com>
2015-03-23 19:10:37 -0400

    Created struct and added data to it

--- battleship_main.cpp
+++ battleship_main.cpp
@@ -1,31 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <iostream>
+#include "battleship_main.h"
 
-using namespace std;
+struct tile values[100]; // Struct Array to hold values
 
-int main(int argc, char *argv[1]){
-	// argc is for checking command line argument existence
-	// argv is for accessing command line arguments
+void populate(){
 
-	// For right now this will create a 10x10 array 
-	// It only fills it with numbers right now though so we need to 
-	// find a way to fill it with a letter and number
 
-	int values[10][10];
-	int count = 0;
+	string number = "0"; // Number to be combined
+	string letter = "A"; // Letter to be combined
+
+	char num = '0'; // temporary use when converting from int to string
+	char two = 'A';	// temporary use when converting from int to string
+
+	for (int i = 1; i < 100; i++)
+	{
+
+		// This statement is used to increment values up every 10 loops
+		// This is how we get A1, A2, A3, etc...
+
+		if ((i % 10) == 0)
+		{
+			int temp = (int)two; // convert temp to an int
+			temp++;				 // increment up one
+			two = (char)temp;	 // convert back to char --- Must use char as midleman between int and string
+			stringstream out;    // stringsream to write and read from
+			out << two;			 // write temp char to stringstream
+			out >> letter;		 // write incremented string from stringsream to string 'letter'
+
+
+			// Same as above but for numbers
+
+			int temp2 = (int)num;
+			temp2++;
+			num = (char)temp2;
+			stringstream out2;
+			out2 << num;
+			out2 >> number;
+
+		}
+
+		// Combines letter and number and assigns it to name in struct[i]
+		values[i].str.append(letter);
+		values[i].str.append(number);
+		values[i].id_num = i;
+	}
+
+}
+
+void print_board(tile_t param[]){
+
+	int count = 0; // keeps track of where to print line breaks
 
 	for (int i = 0; i < 10; i++)
 	{
-		
-		for (int j = 0; j < 9; j++)
+		for (int i = 0; i < 10; i++)
 		{
-			values[i][j] = count;
-			cout << values[i][j] << " ";
+			cout << param[count].str << " ";
 			count++;
 		}
 		cout << "\n";
-
 	}
+}
+
+int main(int argc, char *argv[1]){
+
+	populate();
+	print_board(values);
+
 }
