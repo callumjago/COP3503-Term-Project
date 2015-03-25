68de7dddb917ab25bf72a872fc39f5e54f06549f
Annhiluc <annieluc@ufl.edu>
2015-03-25 18:38:50 -0400

    Made the player.cpp file into a header file.

---
+++ player.h
@@ -0,0 +1,16 @@
+#include <board.h>
+
+class Player{
+	public:
+		virtual bool setShip() = 0;
+		virtual bool guess() = 0;
+
+	private:
+		Board board;
+		Ship carrier;
+		Ship battleship;
+		Ship submarine;
+		Ship cruiser;
+		Ship destroyer;
+		string guessPos;
+}
