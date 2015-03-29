d3a658fe9793cafbb4ec26d3f74ba8b8931a3dcc
unknown <blanchardeion@ufl.edu>
2015-03-29 18:25:33 -0400

    code tidying

--- user.h
+++ user.h
@@ -1,22 +1,21 @@
 #include "player.h"
 
 class User : public Player{
-	private:
+private:
 
-	public:
-		User(){
-			board = new Board();
+public:
+	User(){
+		board = new Board();
 
-			carrier = Ship(5);
-			battleship = Ship(4);
-			submarine = Ship(3);
-			cruiser = Ship(3);
-			destroyer = Ship(2);
-		}
-		bool setShip();
-		bool guess();
-		bool isValidPosition(string desiredPose);
-
+		carrier = Ship(5);
+		battleship = Ship(4);
+		submarine = Ship(3);
+		cruiser = Ship(3);
+		destroyer = Ship(2);
+	}
+	bool setShip();		//Eion: I think these function declarations are redundant sincce they are already declared in player.h?
+	bool guess();		//""
+	bool isValidPosition(string desiredPos);	//""
 };
 	
 
