10530cc4ada7604c50268f0370dd9935a2c36195
unknown <blanchardeion@ufl.edu>
2015-03-31 18:52:28 -0400

    Fleshed out the isValidPosition() function more

--- src/computer.cpp
+++ src/computer.cpp
@@ -2,11 +2,7 @@
 #include <string>
 #include <stdlib.h>
 
-bool Computer::setShip(){
-	return false;
-}
-
-bool Computer::guess(){
+bool Computer :: guess(){
 	int num = rand() % 10 + 1;
 	char let = 'A' + (rand() % 10);
 
@@ -16,6 +12,132 @@ bool Computer::guess(){
 	str >> guess;
 
 	return false;
+}
+
+bool Computer :: setShip(){
+	string desPos = "";
+
+	//guess-like algorithm to randomize position
+	int orient = isValidPosition(desPos, 2);
+	if(orient == -1){ return false; }
+	else if(orient == 0){
+		//set according to up algorithm
+	}
+	else if(orient == 1){
+		//set according to down algorithm
+	}
+	else if(orient == 2){
+		//set according to left algorithm
+	}
+	else if(orient == 3){
+		//set according to right algorithm
+	}
+
+	//repeat for remaining ships
+
+	return true;
+}
+
+int Computer :: isValidPosition(string desPos, int length){
+	char let = desPos.charAt(0);
+	int num = desPos.char(1);
+	if(num == 1 && (int)desPos.charAt(2) == 0){
+		num = 10;
+	}
+
+	char tempLet = 'A';
+	int tempNum = 1;
+
+	bool left = true, right = true, up = true, down = true;
+
+	int ind = ((let - 'A') * 10) + num;
+	int tempInd = 0;
+
+	if(let < 'A' || let > 'J' || num < 1 || num > 10){
+		cout << "Error: invalid position choice." << endl;
+		return -1;
+	}
+
+	if(board.getStatus(ind) != 0){
+		cout << "Error: ship already placed there." << endl;
+		return -1;
+	}
+
+	if(let - length >= 'A'){					//check for "up" orientation
+		for(int i = 0; i <= length; i++){
+			tempLet = let - i;
+			tempNum = num;
+
+			tempInd = ((tempLet - 'A') * 10) + tempNum;
+
+			if(board.getStatus(ind) != 0){
+				up = true;
+			}
+		}
+	}
+
+	if(let - length >= 'A'){					//check if "up" orientation is possible
+		tempLet = let;
+		tempNum = num;
+		for(int i = 0; i < length; i++){
+			tempInd = ((tempLet-- - 'A') * 10) + tempNum;
+
+			if(board.getStatus(tempInd) != 0){ up = false; }
+		}
+	}
+	else{ up = false; }
+
+	if(let + length <= 'J'){					//check if "down" orientation is possible
+		tempLet = let;
+		tempNum = num;
+		for(int i = 0; i < length; i++){
+			tempInd = ((tempLet++ - 'A') * 10) + tempNum;
+
+			if(board.getStatus(tempInd) != 0){ down = false; }
+		}
+	}
+	else{ down = false; }
+
+	if(num - length >= 1){					//check if "left" orientation is possible
+		tempLet = let;
+		tempNum = num;
+		for(int i = 0; i < length; i++){
+			tempInd = ((tempLet - 'A') * 10) + tempNum--;
+
+			if(board.getStatus(tempInd) != 0){ left = false; }
+		}
+	}
+	else{ left = false; }
+
+	if(num + length <= 10){					//check if "right" orientation is possible
+		tempLet = let;
+		tempNum = num;
+		for(int i = 0; i < length; i++){
+			tempInd = ((tempLet - 'A') * 10) + tempNum++;
+
+			if(board.getStatus(tempInd) != 0){ right = false; }
+		}
+	}
+	else{ right = false; }
+
+	bool *option = {up, down, left, right};
+
+	int sum = 0;
+	for(bool op : option){ if(op){ sum++; } }
+
+	if(sum == 0){
+		cout << "Error: no orientation options." << endl;
+		return -1;
+	}
+	
+	int randNum = rand() % 24;
+	int m = 0;
+	while(randNum >= 0){
+		if(option[m++]){ randNum--; }
+		if(m == 4){ m = 0; }
+	}
+
+	return --m;	// -1 means invalid or error, 0 means "up", 1 means "down", 2 means "left", 3 means "right"
 }
 
 int main(){
