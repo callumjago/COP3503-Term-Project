d7741c0b737799776bf2fa49f556e6f787bb83bc
Annhiluc <annieluc@ufl.edu>
2015-04-01 18:09:55 -0400

    Cleaning various files to compile all classes.

--- include/player.h
+++ include/player.h
@@ -6,6 +6,13 @@ public:
 	virtual bool setShip() = 0;
 	virtual bool guess() = 0;
 	virtual int isValidPos(string desPos, int length) = 0;
+	Player(){
+		carrier = Ship(5);
+		battleship = Ship(4);
+		submarine = Ship(3);
+		cruiser = Ship(3);
+		destroyer = Ship(2);
+	}
 
 private:
 		
--- include/user.h
+++ include/user.h
@@ -1,17 +1,17 @@
 #include "player.h"
 
-class User : public Player{
+class User: public Player{
 private:
 
 public:
 	User(){
-		board = new Board();
+		/*board = new Board();
 
 		carrier = Ship(5);
 		battleship = Ship(4);
 		submarine = Ship(3);
 		cruiser = Ship(3);
-		destroyer = Ship(2);
+		destroyer = Ship(2);*/
 	}
 	bool setShip();		//Eion: I think these function declarations are redundant sincce they are already declared in player.h?
 	bool guess();		//""
