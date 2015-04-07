02b0832acd9604658703a29a67f97f2b843fb93a
unknown <blanchardeion@ufl.edu>
2015-04-07 16:02:43 -0400

    Fixed mysterious lines(?)

--- include/player.h
+++ include/player.h
@@ -3,11 +3,9 @@
 class Player{
 
 public:
-<<<<<<< HEAD
 	virtual bool setShip() = 0;
 	virtual bool guess() = 0;
 	virtual int isValidPos(string desPos, int length) = 0;
-=======
 
 	Player(){
 		carrier = new Ship(5);
@@ -16,7 +14,6 @@ public:
 		cruiser = new Ship(3);
 		destroyer = new Ship(2);
 	}
->>>>>>> 1f89c432a1b9c4c181ee51b8bfda2ec9aae0befa
 
 void setCarrier(){
 	std::cout << carrier->getIsSunk();
